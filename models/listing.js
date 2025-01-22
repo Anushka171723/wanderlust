a853787d6ca181671c4580529cf532d2b3e363c9
Anushka Yadav <yadavaka1723@gmail.com>
2025-01-22 18:53:28 +0530

    updated

--- models/listing.js
+++ models/listing.js
@@ -6,11 +6,9 @@ const listingSchema = new Schema({
     type: String,
     required: true,
   },
-  description: {
-    type: String,
-  },
+  description:String,
+  
   image: {
-    url: {
       type: String,
       default:
         "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
@@ -19,27 +17,11 @@ const listingSchema = new Schema({
           ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
           : v,
     },
-    filename: {
-      type: String,
-    },
-  },
-  
-  // image:{
-  //   url:String,
-  //   filename:String,
-  // },
-  price: {
-    type: Number,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
+    
+
+  price:Number,
+  location:String,
+  country:String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
